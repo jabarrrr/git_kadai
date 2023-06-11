4da8d30c1f5e9d90f0d0892fcebeea5231f38131
jabar <ryu89baba@asagi.waseda.jp>
2023-06-11 22:23:08 +0900

    added comment out

--- pattern2.py
+++ pattern2.py
@@ -1,3 +1,4 @@
+#数字当てゲーム
 import random
 
 ans=random.randint(100,999)
@@ -5,6 +6,7 @@ max_count=10
 print('3桁の数字の中から一つ選んだよ。')
 print('その数字を',max_count,'回以内に当ててね。')
 
+#正解or不正解の判定
 for i in range(1,max_count+1):
     print(i,'回目、いくつかな?')
     num=int(input())
